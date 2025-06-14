3881acc90ca603334e09d700826b78e482df6d60
flush36 <thiagoferreira.dev@gmail.com>
2025-06-13 22:47:10 -0300

    incluindo autodocs no storybook

--- libs/storybook-host/src/stories/button.component.stories.ts
+++ libs/storybook-host/src/stories/button.component.stories.ts
@@ -10,5 +10,47 @@ export default meta;
 
 type Story = StoryObj<ButtonComponent>;
 
-export const PrimaryButton: Story = {};
-export const SecondaryButton: Story = {};
+export const PrimaryButton: Story = {
+  args: {
+    text: "Action",
+    variant: "primary",
+    disabled: false
+  }
+};
+
+export const PrimaryButtonDisabled: Story = {
+  args: {
+    ...PrimaryButton.args,
+    disabled: true
+  }
+};
+
+export const SecondaryButton: Story = {
+  args: {
+    ...PrimaryButton.args,
+    variant: "secondary",
+    disabled: false
+  }
+};
+
+export const SecondaryButtonDisabled: Story = {
+  args: {
+    ...SecondaryButton.args,
+    disabled: true
+  }
+};
+
+export const TerciaryButton: Story = {
+  args: {
+    ...PrimaryButton.args,
+    variant: 'tertiary',
+    disabled: false
+  }
+}
+
+export const TerciaryButtonDisabled: Story = {
+  args: {
+    ...TerciaryButton.args,
+    disabled: true
+  }
+}
